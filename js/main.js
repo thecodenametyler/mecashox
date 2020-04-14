55faf7235deef9648e88eaa8faccea4babccb15c
ritesh <ritesh@sugar.mu>
2020-04-14 16:23:49 +0400

    FEATURE(MECASHOX): add maps + other componets

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([-20.2982072, 57.4894003], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-20.2982072, 57.4894003]).addTo(map);
